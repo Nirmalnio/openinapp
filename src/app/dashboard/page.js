06326bb77671790db4bb9a31fab64d5b46e732ac
dineshmartin01 <dineshmartin01@gmail.com>
2023-06-12 11:17:24 +0530

    header added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,12 +2,16 @@
 import React,{useState,useEffect} from 'react'
 import style from "./dashboard.module.css"
 import Sidebar from '@/component/dashboard/sidebar'
+import Dashboardheader from '@/component/dashboard/header/dashboardheader';
 function Dashboard() {
   return (
     <div className={style.DashboardDiv}>
            <div className={style.Dashboardinside}>
            <Sidebar/>
-              
+           <div>
+             <Dashboardheader/>            
+           </div>
+                       
            </div>
     </div>
   )
