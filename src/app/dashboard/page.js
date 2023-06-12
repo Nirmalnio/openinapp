fad8e770c98fe9ad2fe1fa06decfacc6665ef0b0
nirmalnio <nirmalnio123@gmail.com>
2023-06-12 18:57:44 +0530

    fix issues

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,14 +3,22 @@ import React,{useState,useEffect} from 'react'
 import style from "./dashboard.module.css"
 import Sidebar from '@/component/dashboard/sidebar'
 import Dashboardheader from '@/component/dashboard/header/dashboardheader';
+import Chart from '@/component/dashboard/charts/Charts';
+import Circlechart from '@/component/dashboard/charts/CircleChart';
+import Schedulecard from '@/component/dashboard/Schedule/Schedulecard';
 function Dashboard() {
   return (
     <div className={style.DashboardDiv}>
            <div className={style.Dashboardinside}>
            <Sidebar/>
-           <div>
-             <Dashboardheader/>            
-           </div>
+          <div>
+             <Dashboardheader/>  
+             <Chart/>
+             <div className={style.btmsecDiv}> 
+                        <Circlechart/>
+                        <Schedulecard/>
+             </div>
+          </div>
                        
            </div>
     </div>
---
+++ src/component/dashboard/charts/CircleChart.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import style from "./Chart.module.css"
+function Circlechart(){
+    return(
+            <div className={style.circlechartmainDiv}>
+                
+            </div>
+    )
+}
+export default Circlechart;
